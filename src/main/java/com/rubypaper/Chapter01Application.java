95d5989778455853c4ab23c0a4b1cfb06e4a5aec
김성민 <evidentkr@naver.com>
2020-11-28 00:15:25 +0900

    fix

--- src/main/java/com/rubypaper/Chapter01Application.java
+++ src/main/java/com/rubypaper/Chapter01Application.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages= {"com.rubypaper"})
 public class Chapter01Application {
-
+//
 	public static void main(String[] args) {
 		/*
 		 * SpringApplication application = new SpringApplication(Chapter01Application.class);
